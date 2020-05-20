4997a409720f20561cfd728b997d24636f7f2ff7
qttq23 <1753102@student.hcmus.edu.vn>
2020-05-20 08:36:04 +0700

    add routes for login and homepage

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
 
 const express = require('express');
+const expSession = require('express-session');
+var exphbs = require('express-handlebars');
+
+
 const app = express();
+
+// view content of POST message
+app.use(express.urlencoded({
+    extended: true
+})
+);
+
+
+// set handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main.handlebars',
+})
+);
+app.set('view engine', 'handlebars');
 
 
 // set public static folder
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// homepage
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+// set session
+app.use(expSession(
+    {
+        secret: 'keyboard cat',
+        cookie: { maxAge: 60000 * 600 },
+        resave: false,
+        saveUninitialized: true,
+    })
+)
+
+
+
+// authen
+const authenRouter = require('./routes/authen');
+app.use('/authen', authenRouter);
+
+
+// app middleware
+app.use(async function(req, res, next){
+
+    console.log('entrance')
+
+	// if(!req.session.isLogin){
+    if(false){
+        console.log('not yet login');
+
+        res.redirect('/authen/login');
+    }
+    else{
+        console.log('already login');
+        console.log(req.session.account);
+        next();
+    }
+
 })
 
+
+// default get
+app.get('/', function(req, res){
+
+    res.redirect('/home')
+
+})
+
+// homepage
+app.get('/home', function(req, res){
+    // res.sendFile(__dirname + '/index.html');
+    res.render('home', {
+        isLogin: req.session.isLogin,
+        account: req.session.account
+    });
+})
 
 
 // create http server listen to port
---
+++ routes/authen.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+
+const router = express.Router();
+module.exports = router;
+
+
+// login
+router.get('/login', function (req, res) {
+
+    console.log('get login');
+
+    if (!req.session.isLogin) {
+        res.render('authen/login');
+    }
+    else {
+        let url = req.headers.referer || '../home';
+        console.log(`referer: ${req.headers.referer}`);
+        console.log(`redirecting to ${url}`);
+        res.redirect(url);
+    }
+})
+
+router.post('/login', function (req, res) {
+
+    console.log(req.body);
+
+    req.session.isLogin = true;
+    const account = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    req.session.account = account;
+    res.redirect('../home');
+})
