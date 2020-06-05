74d198026aaa01d1ac50d69d607717ce7dcae063
Tran Thuan Thanh <t0942842441@gmail.com>
2020-06-05 08:15:37 +0700

    load data to profile page

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,31 @@
-
 const router = require('express').Router();
 module.exports = router;
 
+const model = require('../models/user.model');
+
 
 // get profile
-router.get('/profile', function (req, res) {
+router.get('/profile', function(req, res) {
 
     console.log('get user/profile');
     console.log(req.query.id);
 
-    const userProfile = model.get(req.query.id);
-    res.render('user/profile', {
-        userProfile: userProfile
-    });
+    (async() => {
+        const userProfile = await model.get_user_by_id(req.query.id);
+        console.log(userProfile);
+
+        res.render('user/profile', {
+            userProfile: userProfile
+        });
+    })();
+
+
 
 });
 
 
 // update profile
-router.post('/profile', function (req, res) {
-
+router.post('/profile', function(req, res) {
     console.log('post user/profile');
     console.log(req.body);
 
@@ -30,12 +36,10 @@ router.post('/profile', function (req, res) {
         res.json({
             result: true
         });
-    }
-    else {
+    } else {
         res.json({
             result: false,
             msg: 'update profile failed.'
         });
     }
 });
-
