fcc843f811ea38f2f6a961c396473d706929672e
Tran Thuan Thanh <t0942842441@gmail.com>
2020-06-15 20:14:26 +0700

    fix add user

--- models/room.model.js
+++ models/room.model.js
@@ -2,7 +2,7 @@ const db = require('../utils/database.util');
 
 module.exports = {
     all: isCurrent => db.query(`call get_room_all(${isCurrent})`),
-    add: (room) => db.query(`call add_room("${room}")`),
+    add: (roomId, roomName, hostId, startTime, endTime) => db.query(`call add_room("${roomId}","${roomName}","${hostId}","${startTime}","${endTime}")`),
     remove: (roomId) => db.query(`call remove_room("${userId}","${friendId}")`),
     get_room_by_id: (userId, isCurrent) => db.query(`call get_room_by_id("${userId}","${isCurrent}")`).then(result => result[0]),
     get_room_by_host_id: (userId, isCurrent) => db.query(`call get_room_by_host_id("${userId}","${isCurrent}")`).then(result => result[0]),
