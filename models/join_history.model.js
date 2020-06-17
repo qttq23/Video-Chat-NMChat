d353dbb7f317adecf5639b36cc7099f1327e33bf
Tran Thuan Thanh <t0942842441@gmail.com>
2020-06-17 21:16:40 +0700

    add update models

--- models/join_history.model.js
+++ models/join_history.model.js
@@ -2,7 +2,8 @@ const db = require('../utils/database.util');
 
 module.exports = {
     all: isCurrent => db.query(`call get_join_history_all(${isCurrent})`),
-    update: (userId, roomId, joinTime, leaveTime) => db.query(`call update_join_history_leave_time(${userId},${roomId},${joinTime},${leaveTime})`),
+    //update: (userId, roomId, joinTime, leaveTime) => db.query(`call update_join_history(${userId},${roomId},${joinTime},${leaveTime})`),
+    update_leave_time: (userId, roomId, leaveTime) => db.query(`call update_join_history_leave_time(${userId},${roomId},${leaveTime})`),
     add: (userId, roomId, joinTime, leaveTime) => db.query(`call add_join_history("${userId}","${roomId}","${joinTime}","${leaveTime}")`),
     get_user_by_id: userId => db.query(`call get_join_history_by_id("${userId}")`).then(result => result[0]),
     get_by_rooom_id: roomId => db.query(`call get_join_history_by_room_id("${roomId}")`).then(result => result[0]),
--- referenceUpdateUser.js
+++ referenceUpdateUser.js
@@ -8,18 +8,36 @@ const roomModel = require('./models/room.model');
 //     "",
 //     "1995-03-16 00:00:00",
 //     "013123123222"))());
-console.log((async() => await model.add(
-    "63b1255c-88dc-4710-a314-770188709916",
-    "Tran Thuan Thanh",
-    "tranthu123@gmail.com",
-    "123",
-    "",
-    "1995-03-16 00:00:00",
-    "013123123222"))());
+// console.log((async() => await model.add(
+//     "63b1255c-88dc-4710-a314-770188709916",
+//     "Tran Thuan Thanh",
+//     "tranthu123@gmail.com",
+//     "123",
+//     "",
+//     "1995-03-16 00:00:00",
+//     "013123123222"))());
 
-console.log((async() => await roomModel.add(
-    "63b1255c-88dc-4710-a314-770188709916",
-    "Tran Thuan Thanh",
-    "63b1255c-88dc-4710-a314-770188709995",
-    "1995-03-16 00:00:00",
-    ""))());
+// console.log((async() => await roomModel.add(
+//     "63b1255c-88dc-4710-a314-770188709917",
+//     "Tran Thuan Thanh",
+//     "63b1255c-88dc-4710-a314-770188709995",
+//     "1995-03-16 00:00:00",
+//     ""))());
+
+
+
+// console.log((async() => await roomModel.add(
+//         "63b1255c-88dc-4710-a314-770188709917",
+//         "Tran Thuan Thanh",
+//         "63b1255c-88dc-4710-a314-770188709995",
+//         "1995-03-16 00:00:00",
+//         ""))());
+
+
+console.log((async() => await roomModel.update_room_end_time(
+    "06711889-771b-43c8-8ac7-4ee08a33fde6",
+    "0ed165cf-ac95-4da4-8a65-54d8ea99719b",
+    "2020-06-16 18:05:13"))());
+
+
+//CALL update_room_end_time(,"0ed165cf-ac95-4da4-8a65-54d8ea99719b","2020-06-16 18:05:13");
