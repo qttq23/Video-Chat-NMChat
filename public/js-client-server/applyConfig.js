9989150d6f419f713400d36715c00e9c5e1725e4
qttq23 <1753102@student.hcmus.edu.vn>
2020-06-20 20:57:20 +0700

    add more ui videos, fixed size for video

--- public/js-client-server/applyConfig.js
+++
@@ -1,5 +0,0 @@
-$(()=>{
-
-    $('head').append('<link rel="icon" type="image/png" href="/images/icons/blue_video_camera.png"/>');
-    // $('head title').html('updated');
-});
--- public/js-client-server/client-config.js
+++ public/js-client-server/client-config.js
@@ -1,11 +1,11 @@
 var config = {
     Title: 'NMChat',
     IconUrl: '/images/icons/blue_video_camera.png',
-    BodyBackgroundColor: 'yellow',//'#329AD6',
+    BodyBackgroundColor: 'white',//'#329AD6',
 
-    RoomBackgroundColor: 'red',
+    RoomBackgroundColor: 'darkgrey',
+    ChatPanelBackgroundColor: 'white',
     DefaultVideoPoster: '/images/icons/default_user.png',
-    ChatPanelBackgroundColor: 'green',
     MaxFileSizeUpload: 10485760, // equal 10 MB
 };
 
@@ -18,12 +18,21 @@ $(() => {
 
 
 
-    $('.video-call').attr('poster', config.DefaultVideoPoster);
+    
     $('#panelVideo').css('background-color', config.RoomBackgroundColor);
     $('#bodyRoom').css('background-color', config.RoomBackgroundColor);
     $('#panelChat').css('background-color', config.ChatPanelBackgroundColor);
     $('#panelInput').css('background-color', config.ChatPanelBackgroundColor);
     
+    // append some remote video elements
+    let n = 20;
+    let htmlVideo = 
+    '<video class="video-call remoteVideo" id="remote-video-4" autoplay playsinline></video>';
+    for(i = 0; i < n; i++){
+        $('.video-call-parent').append(htmlVideo);
+    }
+    // set poster for videos
+    $('.video-call').attr('poster', config.DefaultVideoPoster);
 });
 
 
